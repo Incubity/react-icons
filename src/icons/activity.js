705345b9b3feb6416620b688c4c9423af72c786d
Willem <willem@sweetyams.com>
2018-01-05 10:42:13 -0500

    Test of activity.js

--- src/icons/activity.js
+++ src/icons/activity.js
@@ -9,14 +9,13 @@ const Activity = props => {
       width={size}
       height={size}
       viewBox="0 0 24 24"
-      fill="none"
-      stroke={color}
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
       {...otherProps}
     >
-      <polyline points="22 12 18 12 15 21 9 3 6 12 2 12" />
+      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g fill={color} fill-rule="nonzero">
+            <polygon points="15 19 9 1 5.63962039 11.0811388 2 11.0811388 2 12.0811388 6.36037961 12.0811388 9 4.16227766 15 22.1622777 18.3603796 12.0811388 22 12.0811388 22 11.0811388 17.6396204 11.0811388"></polygon>
+        </g>
+    </g>
     </svg>
   );
 };
