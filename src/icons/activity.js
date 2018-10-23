caa420c8d70879b307fb78721d48417ebd3e5a4f
Donovan Charpin <donovan.charpin@gmail.com>
2018-10-23 12:50:09 -0400

    Updated feather icons with new ones

--- src/icons/activity.js
+++ src/icons/activity.js
@@ -11,10 +11,12 @@ const Activity = props => {
       viewBox="0 0 24 24"
       fill="none"
       stroke={color}
-      strokeWidth="1"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...otherProps}
     >
-      <polyline points="2 13 6 13 9 3 15 22 18 13 22 13" />
+      <polyline points="22 12 18 12 15 21 9 3 6 12 2 12" />
     </svg>
   );
 };
--- src/icons/chair.js
+++ src/icons/chair.js
@@ -5,15 +5,11 @@ const Chair = props => {
   const { color, size, ...otherProps } = props;
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={size}
-      height={size}
+      width="24px"
+      height="24px"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke={color}
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
       {...otherProps}
     >
       <path
